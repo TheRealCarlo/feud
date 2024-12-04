1c7580670f896fcc603a500f656548f6eba9087e
TheRealCarlo <carlo@diversifi.app>
2024-12-04 15:05:14 -0600

    commit for page

--- app/page.tsx
+++ app/page.tsx
@@ -71,6 +71,7 @@ export default function Home() {
           <BearInventory
             nfts={nfts}
             userFaction={userFaction}
+            walletAddress={walletAddress}
           />
         );
       case 'history':
