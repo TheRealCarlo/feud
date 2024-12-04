9ab84844a53dc88d98ef79f06dc8cbbc7341d54a
TheRealCarlo <carlo@diversifi.app>
2024-12-04 14:15:32 -0600

    commit for index

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,3 +1,10 @@
+import { useState } from 'react';
+import { BrowserProvider } from 'ethers';
+import { WalletConnect } from '../components/WalletConnect';
+import { GameBoard } from '../components/GameBoard';
+import { BearInventory } from '../components/BearInventory';
+import { Faction } from '../types/game';
+
 export default function Home() {
     const [provider, setProvider] = useState<BrowserProvider | null>(null);
     const [walletAddress, setWalletAddress] = useState<string>('');
