7a350e2a8813720027d3517ba7fd5f3c46bee371
TheRealCarlo <carlo@diversifi.app>
2024-12-04 09:28:25 -0600

    commit for feud v0.2.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Toaster } from 'react-hot-toast';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} bg-black text-white antialiased`}>
         {children}
+        <Toaster position="bottom-right" />
       </body>
     </html>
   )
